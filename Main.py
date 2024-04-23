23b050284cdd7ab96f7145f0822ac902cf3c59e3
JoseDavidVindas <jose.chavarria34@ulatina.net>
2024-04-22 20:47:13 -0600

    Se sube el proyecto

---
+++ Main.py
@@ -0,0 +1,12 @@
+lista = ["g","w","s","r","a","b"]
+lista.sort()
+lista.reverse()
+mi_lista = ["a",1.25,3,"c","error"]
+medios_transporte= ["avión", "auto", "barco", "bicicleta"]
+medios_transporte.append("motocicleta")
+frutas = ["manzana", "banana", "mango", "cereza", "sandía"]
+eliminado = frutas.pop(2)
+print(lista)
+print(mi_lista)
+print(medios_transporte)
+print(frutas)
